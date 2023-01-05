b9531359953d029ac2555ade0eb49b86c29ee38e
IBRAHIM AYAZ <75534198+ibrahimayaz95@users.noreply.github.com>
2023-01-05 21:50:07 +0100

    Add files via upload

---
+++ 09-VOLUME/volume/volume.c
@@ -0,0 +1,66 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: IBRAHIM AYAZ
+// Create Date: 27.12.2022 17:36:00
+// Design Name: volume
+//
+// Description: Program for handling file pointers that we can read data
+// from a file and modify the data then write the modified data to a new file.
+// All the details about the LAB are described at
+// the following link:
+// https://cs50.harvard.edu/x/2022/labs/4/#implementation-details
+//
+// Output: The output should write a file called output.wav with the multiplied
+// data read from the input.wav.
+//////////////////////////////////////////////////////////////////////////////////
+// Modifies the volume of an audio file
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    // TODO: Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
+
+    // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    while(fread(&buffer, sizeof(int16_t), 1, input))
+    {
+        buffer = buffer * factor;
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
+
+    // Close files
+    fclose(input);
+    fclose(output);
+}
